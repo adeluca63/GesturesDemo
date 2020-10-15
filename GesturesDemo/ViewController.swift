b39f56db37bf4bb61ccf72a7991051f55c1cc544
adeluca63 <aidanjohndeluca@gmail.com>
2020-10-15 10:31:06 -0400

    MVP

--- GesturesDemo/ViewController.swift
+++ GesturesDemo/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myIV: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-let Yo = "Yo"
+    
+    @IBAction func IVTapped(_ sender: UITapGestureRecognizer)
+    {
+        print ("Spiderman Tapped")
+    }
+    @IBAction func emojiPanned(_ sender: UIPanGestureRecognizer)
+    {
+        // 1
+         let translation = sender.translation(in: view)
 
+         // 2
+         guard let gestureView = sender.view else
+         {
+           return
+         }
+
+         gestureView.center = CGPoint(
+           x: gestureView.center.x + translation.x,
+           y: gestureView.center.y + translation.y
+         )
+
+         // 3
+         sender.setTranslation(.zero, in: view)
+    }
+    
 }
 
